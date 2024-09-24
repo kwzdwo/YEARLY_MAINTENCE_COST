2a97a30baf4ad54078051963ef1ef5987f368bcb
KojoMensah <kwadwom200@gmail.com>
2024-09-23 23:07:47 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+      double springCost = 0;
+      double  summerCost = 0;
+      double fallCost = 0;
+      double winterCost = 0;
+      double totalCost = 0;
+
+        System.out.println("Enter the maintenance cost for Spring: ");
+
+        System.out.println("Enter the maintenance cost for Summer: ");
+
+        System.out.println("Enter the maintenance cost for Fall: ");
+
+        System.out.println("Enter the maintenance cost for Winter: ");
+
+        totalCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("Total yearly maintenance cost is: $");
+
+    }
+}
